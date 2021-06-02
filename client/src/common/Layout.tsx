e3c2dc668fc99662155a841cf3086e1f225f5b0b
Armianto Sumitro <armiantos@gmail.com>
2021-06-01 21:34:10 -0600

    Add sidebar

--- client/src/common/Layout.tsx
+++ client/src/common/Layout.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 import { makeStyles, ThemeProvider } from '@material-ui/core/styles';
 
 import Header from './Header';
 import theme from '../styles/theme';
 
+const drawerWidth = 240;
+
 const useStyles = makeStyles((theme) => ({
     root: {
         margin: theme.spacing(2),
         maxWidth: '100%',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    content: {
+        marginLeft: drawerWidth,
     },
 }));
 
@@ -17,8 +33,25 @@ const Layout: React.FC = ({ children }) => {
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
-            <Header />
-            <div className={classes.root}>{children}</div>
+            <nav className={classes.drawer}>
+                <Drawer
+                    classes={{
+                        paper: classes.drawerPaper,
+                    }}
+                    variant="permanent"
+                    open
+                >
+                    <List>
+                        <ListItem button>
+                            <ListItemText>Schedule</ListItemText>
+                        </ListItem>
+                    </List>
+                </Drawer>
+            </nav>
+            <main className={classes.content}>
+                <Header />
+                <div className={classes.root}>{children}</div>
+            </main>
         </ThemeProvider>
     );
 };
