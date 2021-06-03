644218f08a73e964baad28edd6e273758dd66366
Armianto Sumitro <armiantos@gmail.com>
2021-06-03 07:03:02 -0600

    Refactor planner to source events from redux

--- client/src/pages/Planner.tsx
+++ client/src/pages/Planner.tsx
@@ -3,8 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 import CourseSearcher from '../components/CourseSearcher';
 import ScheduleComponent from '../components/Schedule';
-import Event, { Day } from '../data/Event';
-import getRandomColor from '../util/getRandomColor';
+import { useAppSelector } from '../store';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -15,82 +14,7 @@ const useStyles = makeStyles((theme) => ({
 
 const Planner = () => {
     const classes = useStyles();
-
-    const events: Event[] = [
-        {
-            startTime: {
-                hour: 9,
-            },
-            endTime: {
-                hour: 9,
-                minute: 50,
-            },
-            days: [Day.Monday, Day.Wednesday, Day.Friday],
-            color: getRandomColor(),
-            description: 'ECE 420',
-        },
-        {
-            startTime: {
-                hour: 11,
-            },
-            endTime: {
-                hour: 11,
-                minute: 50,
-            },
-            days: [Day.Monday, Day.Wednesday, Day.Friday],
-            color: getRandomColor(),
-            description: 'CMPUT 366',
-        },
-        {
-            startTime: {
-                hour: 12,
-            },
-            endTime: {
-                hour: 12,
-                minute: 50,
-            },
-            days: [Day.Monday, Day.Wednesday, Day.Friday],
-            color: getRandomColor(),
-            description: 'ECE 422   ',
-        },
-        {
-            startTime: {
-                hour: 14,
-            },
-            endTime: {
-                hour: 14,
-                minute: 50,
-            },
-            days: [Day.Monday, Day.Wednesday, Day.Friday],
-            color: getRandomColor(),
-            description: 'ECE 493',
-        },
-        {
-            startTime: {
-                hour: 9,
-                minute: 20,
-            },
-            endTime: {
-                hour: 10,
-                minute: 50,
-            },
-            days: [Day.Tuesday, Day.Thursday],
-            color: getRandomColor(),
-            description: 'CMPUT 325',
-        },
-        {
-            startTime: {
-                hour: 14,
-            },
-            endTime: {
-                hour: 16,
-                minute: 50,
-            },
-            days: [Day.Thursday],
-            color: getRandomColor(),
-            description: 'ECE 420',
-        },
-    ];
+    const events = useAppSelector((state) => state.schedule.events);
 
     return (
         <div className={classes.root}>
