ee407d6833ccaa43ad72013c5097fae45a6e6c12
Armianto Sumitro <armiantos@gmail.com>
2021-06-02 23:22:33 -0600

    Only show hourly timeline on the first column

--- client/src/components/DayColumn.tsx
+++ client/src/components/DayColumn.tsx
@@ -1,31 +1,23 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 
 import Event, { Day } from '../data/Event';
 import EventComponent from './Event';
-import theme from '../styles/theme';
 
 type DayProps = {
     day: Day;
     events: Event[];
+    startOfDay: number;
+    hourHeight: number;
 };
-
-const hourHeight = theme.spacing(8);
 
 const useStyles = makeStyles((theme) => ({
     root: {
         width: '100%',
         position: 'relative',
     },
-    header: {
-        marginBottom: theme.spacing(4),
-    },
     day: {
         display: 'flex',
-    },
-    hour: {
-        height: hourHeight,
     },
     timelotsColumn: {
         marginRight: theme.spacing(2),
@@ -36,32 +28,12 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-function addMeridiemSuffix(hour: number) {
-    return hour / 12 < 1 ? `${hour} AM` : hour === 12 ? `${hour} PM ` : `${hour % 12} PM`;
-}
-
-const DayColumn = ({ day, events }: DayProps) => {
+const DayColumn = ({ day, events, startOfDay, hourHeight }: DayProps) => {
     const classes = useStyles();
-
-    const startOfDay = 8;
-    const endOfDay = 22;
-
-    const hours = [...Array(endOfDay - startOfDay).keys()].map((hour: number) => hour + startOfDay);
 
     return (
         <div key={day} className={classes.root}>
-            <Typography align="center" className={classes.header}>
-                {Day[day]}
-            </Typography>
-
             <div key={day} className={`${classes.root} ${classes.day}`}>
-                <div className={classes.timelotsColumn}>
-                    {hours.map((hour) => (
-                        <div key={hour} className={classes.hour}>
-                            <Typography align="right">{addMeridiemSuffix(hour)}</Typography>
-                        </div>
-                    ))}
-                </div>
                 <div className={classes.eventColumn}>
                     {events.map((event) => {
                         const start = event.startTime.hour + (event.startTime.minute || 0) / 60;
--- client/src/components/Schedule.tsx
+++ client/src/components/Schedule.tsx
@@ -1,32 +1,78 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
 import Event, { Day } from '../data/Event';
 import DayColumn from './DayColumn';
+import addMeridiemSuffix from '../util/addMeridiemSuffix';
+import theme from '../styles/theme';
 
 export type ScheduleProps = {
     events: Event[];
 };
 
+const hourHeight = theme.spacing(8);
+
 const useStyles = makeStyles((theme) => ({
     root: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    week: {
         display: 'grid',
         gap: theme.spacing(2),
-        gridTemplateColumns: 'repeat(5, 1fr)',
+        gridTemplateColumns: '5rem repeat(5, 1fr)',
         height: '100%',
     },
-    column: {},
+    header: {
+        marginTop: theme.spacing(1),
+        marginBottom: theme.spacing(3),
+    },
+    hour: {
+        height: hourHeight,
+    },
+    timelotsColumn: {
+        marginRight: theme.spacing(2),
+    },
 }));
 
 const Schedule = ({ events }: ScheduleProps) => {
     const classes = useStyles();
     const days = [Day.Monday, Day.Tuesday, Day.Wednesday, Day.Thursday, Day.Friday];
 
+    const startOfDay = 8;
+    const endOfDay = 22;
+
+    const hours = [...Array(endOfDay - startOfDay).keys()].map((hour: number) => hour + startOfDay);
+
     return (
         <div className={classes.root}>
-            {days.map((day) => (
-                <DayColumn day={day} events={events.filter((event) => event.days.includes(day))} />
-            ))}
+            <div className={classes.week}>
+                <div></div>
+                {days.map((day) => (
+                    <Typography align="center" className={classes.header}>
+                        {Day[day]}
+                    </Typography>
+                ))}
+            </div>
+            <div className={classes.week}>
+                <div className={classes.timelotsColumn}>
+                    {hours.map((hour) => (
+                        <div key={hour} className={classes.hour}>
+                            <Typography align="right">{addMeridiemSuffix(hour)}</Typography>
+                        </div>
+                    ))}
+                </div>
+
+                {days.map((day) => (
+                    <DayColumn
+                        day={day}
+                        events={events.filter((event) => event.days.includes(day))}
+                        startOfDay={startOfDay}
+                        hourHeight={hourHeight}
+                    />
+                ))}
+            </div>
         </div>
     );
 };
